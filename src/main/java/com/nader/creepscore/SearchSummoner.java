ca2e7e980b6049bd1fb3e89103f736cbf05ef9e8
Nader <naderb1992@gmail.com>
2016-10-17 14:44:32 -0400

    Added form with Echo Text Label

--- src/main/java/com/nader/creepscore/SearchSummoner.java
+++ src/main/java/com/nader/creepscore/SearchSummoner.java
@@ -4,9 +4,17 @@ import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxFallbackLink;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.*;
+import org.apache.wicket.markup.html.form.Button;
+import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+import java.awt.*;
+
 
 /**
  * Created by nader.baradar on 10/14/2016.
@@ -16,13 +24,33 @@ import org.apache.wicket.request.mapper.parameter.PageParameters;
 public class SearchSummoner extends WebPage{
     private static final long serialVersionUID = 1L;
     int ajaxCounter;
-    Label ajaxLabel;
+    Label ajaxLabel, summonerInfo;
+    Form summonerSearchForm;
+    TextField summonerSearchBox;
 
     public SearchSummoner(final PageParameters parameters){
         super(parameters);
         int visitCount = 0;
 
+        //ADD VISIT COUNT LABEL
         add(new Label("visitCounter", HomePage.visitCounter));
+
+        //ADD FORM COMPONENT
+        summonerSearchForm= new Form("summonerSearchForm");
+        summonerSearchBox = new TextField("summonerSearchBox", new Model(""));
+        summonerSearchForm.add(summonerSearchBox);
+        summonerSearchForm.add(new Button("summonerSearchButton") {
+            @Override
+            public void onSubmit(){
+                String value = (String)summonerSearchBox.getModelObject();
+                summonerInfo.setDefaultModelObject(value);
+                summonerSearchBox.setModelObject("");
+            }
+        });
+        add(summonerSearchForm);
+        add(summonerInfo = new Label("summonerInfo", new Model("")));
+
+        //ADD AJAX LINK AND DYNAMIC LABEL
         add(new AjaxFallbackLink("ajaxCounterLink") {
             @Override
             public void onClick(AjaxRequestTarget target){
@@ -36,6 +64,7 @@ public class SearchSummoner extends WebPage{
         ajaxLabel.setOutputMarkupId(true);
         add(ajaxLabel);
 
+        //ADD HOME BUTTON
         add(new Link("homeLink") {
             @Override
             public void onClick(){
