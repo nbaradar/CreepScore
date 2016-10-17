92f73c8cb8076ba4d60904ccdb1ea7cc86520429
Nader <naderb1992@gmail.com>
2016-10-17 16:49:06 -0400

    Added form with Echo Text Label

--- src/main/java/com/nader/creepscore/SearchSummoner.java
+++ src/main/java/com/nader/creepscore/SearchSummoner.java
@@ -8,7 +8,6 @@ import org.apache.wicket.markup.html.form.*;
 import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.link.Link;
-import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
